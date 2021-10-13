696c5bb8b9b4451f9ee079b5813824e192b46c25
ZoeyTheEgoist <jragon.alt1@gmail.com>
2021-10-13 17:04:20 +0800

    added basic character and default game mode

--- Source/ProjectGlow/PlayerCharacter.cpp
+++ Source/ProjectGlow/PlayerCharacter.cpp
@@ -6,10 +6,48 @@
 // Sets default values
 APlayerCharacter::APlayerCharacter()
 {
- 	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+ 	// Set this character to call Tick() every frame.
 	PrimaryActorTick.bCanEverTick = true;
 
+	// Set half height and radius of capsule
+	GetCapsuleComponent()->InitCapsuleSize(42.0f, 96.0f);
+
+	// Forbids character to rotate
+	bUseControllerRotationPitch = false;
+	bUseControllerRotationYaw = false;
+	bUseControllerRotationRoll = false;
+
+	// Allow the camera to rotate
+	GetCharacterMovement()->bOrientRotationToMovement = true;
+	GetCharacterMovement()->RotationRate = FRotator(0.0f, 540.0f, 0.0f);
+
+	// Attach Camera Boom to Player
+	CameraBoom = CreateDefaultSubobject<USpringArmComponent>(TEXT("Camera Boom"));
+	CameraBoom->SetupAttachment(RootComponent);
+	CameraBoom->TargetArmLength = 0.0f;
+	CameraBoom->bUsePawnControlRotation = true;
+	
+	//Add Camera
+	FollowCamera = CreateDefaultSubobject<UCameraComponent>(TEXT("Follow Camera"));
+	FollowCamera->SetupAttachment(CameraBoom, USpringArmComponent::SocketName);
+	FollowCamera->bUsePawnControlRotation = false; //camera doesn't rotate relative to the arm
+
+	// Initial Properties
+	bDead = false;
+
+	// Character Jumping Stats & Gravity
+	GetCharacterMovement()->JumpZVelocity = 500.0f;
+	GetCharacterMovement()->AirControl = 0.5f;
+	JumpGravity = 2;
+	FallGravity = 4;
+
+	// Character Speeds
+	WalkSpeed = 400;
+	SprintSpeed = 650;
+	GetCharacterMovement()->MaxWalkSpeed = WalkSpeed;
+
 }
+
 
 // Called when the game starts or when spawned
 void APlayerCharacter::BeginPlay()
@@ -23,6 +61,17 @@ void APlayerCharacter::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
 
+	// Check if player is falling
+	if ((GetCharacterMovement()->IsFalling() == true) && (GetVelocity().Z <= 0) && (GetCharacterMovement()->GravityScale != FallGravity)) {
+		// Raise the gravity
+		//GEngine->AddOnScreenDebugMessage(-1, 15.0f, FColor::Yellow, TEXT("Falling!"));
+		GetCharacterMovement()->GravityScale = FallGravity;
+	}
+	else if (GetCharacterMovement()->GravityScale != JumpGravity) {
+		// Reset gravity when on ground
+		GetCharacterMovement()->GravityScale = JumpGravity;
+	}
+
 }
 
 // Called to bind functionality to input
@@ -30,5 +79,61 @@ void APlayerCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputCom
 {
 	Super::SetupPlayerInputComponent(PlayerInputComponent);
 
+	// Look around
+	PlayerInputComponent->BindAxis("Turn", this, &APawn::AddControllerYawInput);
+	PlayerInputComponent->BindAxis("LookUp", this, &APawn::AddControllerPitchInput);
+
+	// Move around
+	PlayerInputComponent->BindAxis("MoveFoward", this, &APlayerCharacter::MoveFoward);
+	PlayerInputComponent->BindAxis("MoveRight", this, &APlayerCharacter::MoveRight);
+
+	// Jump
+	PlayerInputComponent->BindAction("Jump", IE_Pressed, this, &ACharacter::Jump);
+	PlayerInputComponent->BindAction("Jump", IE_Released, this, &ACharacter::StopJumping);
+
+	//Sprint
+	PlayerInputComponent->BindAction("Sprint", IE_Pressed, this, &APlayerCharacter::Sprint);
+	PlayerInputComponent->BindAction("Sprint", IE_Released, this, &APlayerCharacter::StopSprinting);
+}
+
+
+
+void APlayerCharacter::MoveFoward(float Axis)
+{
+	if (!bDead) {
+		// Move relative to the direction you're facing
+		const FRotator Rotation = Controller->GetControlRotation();
+		const FRotator YawRotation(0, Rotation.Yaw, 0);
+		const FVector Direction = FRotationMatrix(YawRotation).GetUnitAxis(EAxis::X);
+		AddMovementInput(Direction, Axis);
+	}
+}
+
+void APlayerCharacter::MoveRight(float Axis)
+{
+	if (!bDead) {
+		// Move relative to the direction you're facing
+		const FRotator Rotation = Controller->GetControlRotation();
+		const FRotator YawRotation(0, Rotation.Yaw, 0);
+
+		const FVector Direction = FRotationMatrix(YawRotation).GetUnitAxis(EAxis::Y);
+		AddMovementInput(Direction, Axis);
+	}
+}
+
+void APlayerCharacter::Sprint()
+{
+	if (!bDead) {
+		GEngine->AddOnScreenDebugMessage(-1, 15.0f, FColor::Yellow, TEXT("Sprinting"));
+		GetCharacterMovement()->MaxWalkSpeed = SprintSpeed;
+	}
+}
+
+void APlayerCharacter::StopSprinting()
+{
+	if (!bDead) {
+		GEngine->AddOnScreenDebugMessage(-1, 15.0f, FColor::Yellow, TEXT("Stopped Sprinting"));
+		GetCharacterMovement()->MaxWalkSpeed = WalkSpeed;
+	}
 }
 
--- Source/ProjectGlow/PlayerCharacter.h
+++ Source/ProjectGlow/PlayerCharacter.h
@@ -4,6 +4,13 @@
 
 #include "CoreMinimal.h"
 #include "GameFramework/Character.h"
+#include "Camera/CameraComponent.h"
+#include "Components/CapsuleComponent.h"
+#include "Components/StaticMeshComponent.h"
+#include "Components/InputComponent.h"
+#include "GameFramework/CharacterMovementComponent.h"
+#include "GameFramework/Controller.h"
+#include "GameFramework/SpringArmComponent.h"
 #include "PlayerCharacter.generated.h"
 
 UCLASS()
@@ -14,6 +21,33 @@ class PROJECTGLOW_API APlayerCharacter : public ACharacter
 public:
 	// Sets default values for this character's properties
 	APlayerCharacter();
+
+	// Camera Properties
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Camera)
+		USpringArmComponent* CameraBoom;
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Camera)
+		UCameraComponent* FollowCamera;
+
+	// Movement Properties
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Movement)
+		float JumpGravity;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Movement)
+		float FallGravity;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Movement)
+		int WalkSpeed;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Movement)
+		int SprintSpeed;
+
+	// Other Properties
+	bool bDead;
+
+	// Movement Methods
+	void MoveFoward(float Axis);
+	void MoveRight(float Axis);
+	void Sprint();
+	void StopSprinting();
+	
+
 
 protected:
 	// Called when the game starts or when spawned
